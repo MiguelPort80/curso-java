544a7a4bc38ed4266858a57e2c47b65be8dce385
MiguelPort80 <miguelangelojulioporto@gmail.com>
2026-04-27 11:54:15 -0300

    JPA repository, dependency injection, database seeding

---
+++ workshop-springboot4-jpa-main/src/main/java/com/educandoweb/course/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entites.User;
+//jpa gera o codigo automaticamente :p
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
