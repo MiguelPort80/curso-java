26efc0948bc0318e341ffb6218186eff3cc025b7
MiguelPort80 <miguelangelojulioporto@gmail.com>
2026-05-06 11:15:28 -0300

    category

---
+++ workshop-springboot4-jpa-main/src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entites.User;
+import com.educandoweb.course.repository.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	//faz injeção automatica com autowired assim
+	/*public UserService(UserRepository repository) {
+		this.repository = repository;
+	}
+	public UserService() {}*/
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
